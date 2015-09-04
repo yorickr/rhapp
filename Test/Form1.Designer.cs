ac9b475e6de606ab71948439d5f68ce2b3a4159e
Yorick Rommers <yorick-rommers@hotmail.com>
2015-09-04 13:18:45 +0200

    progress

--- Test/Form1.Designer.cs
+++ Test/Form1.Designer.cs
@@ -33,6 +33,17 @@
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -80,11 +91,118 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(52, 58);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Time";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(172, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(74, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(52, 88);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Distance";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(52, 118);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Energy";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(172, 90);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(74, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(172, 120);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(74, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(252, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "W";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(252, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "MM:SS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(252, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Km";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(252, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "kJ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(630, 376);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
@@ -104,6 +222,17 @@
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Test/Form1.cs
+++ Test/Form1.cs
@@ -16,6 +16,7 @@ namespace Test
     public partial class Form1 : Form
     {
         private delegate void SetTextDeleg(string data);
+        private delegate void SetTextCallback(string text);
         private SerialPort port;
 
         public Form1()
@@ -44,6 +45,16 @@ namespace Test
                 port.Open();
 
                 port.DataReceived += DataReceivedHandler;
+                BackgroundWorker worker = new BackgroundWorker();
+                worker.DoWork += (o, args) =>
+                {
+                    while (true)
+                    {
+                        sendMessage("ST");
+                        Thread.Sleep(1000);
+                    }
+                };
+                worker.RunWorkerAsync();
 
             }
             catch (Exception ex)
@@ -56,33 +67,109 @@ namespace Test
         public void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
             SerialPort sp = (SerialPort)sender;
-            System.Threading.Thread.Sleep(500);
+            Thread.Sleep(500);
             string indata = sp.ReadExisting();
-            this.BeginInvoke(new SetTextDeleg(DisplayToUI), new object[] { indata });
+            string[] search = new string[] { "ACK", "ERROR", "RUN" };
 
+            bool toUI = true;
+            foreach (string s in search)
+            {
+                if (!s.Contains(indata))
+                {
+                    toUI = false;
+                    break;
+                }
+                else
+                {
+                    toUI = true; 
+                    
+                }
+;
+                
+               
+            }
+            if (!toUI)
+            {
+                UpdateStatus(indata);
+            }
+            else
+            {
+                this.BeginInvoke(new SetTextDeleg(DisplayToUI), new object[] { indata });
+            }
+
+        }
+
+        public void UpdateStatus(string indata)
+        {
+
+            if (label5.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(UpdateStatus);
+                Invoke(d, new object[] { indata });
+            }
+            else
+            {
+                label5.Text = indata.Replace("\t", "    ");
+            }
+            
         }
 
         private void DisplayToUI(string displayData)
         {
-            richTextBox1.Text += displayData;
+            richTextBox1.AppendText(displayData);
+            richTextBox1.ScrollToCaret();
 
+        }
+
+        private void sendMessage(string s)
+        {
+            port.WriteLine(s);
+        }
+
+        private void sendMultipleMessages(string[] s)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += (o, args) =>
+            {
+                foreach (string str in s)
+                {
+                    sendMessage(str);
+                    Thread.Sleep(500);
+                }
+            };
+            worker.RunWorkerAsync();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            port.WriteLine("RS");
+            sendMessage("RS");
             richTextBox1.Text = "";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            port.WriteLine("ST");
+            sendMessage("ST");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            port.WriteLine("CM\nPM " + textBox1.Text);
-            richTextBox1.Text += "PM " + textBox1.Text;
+            sendMultipleMessages(new string[] { "CM", "PW " + textBox1.Text });
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sendMultipleMessages(new string[] { "CM", "PT  " + textBox2.Text });
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            sendMultipleMessages(new string[] { "CM", "PD  " +  Convert.ToDouble( textBox3.Text)*10 });
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            sendMultipleMessages(new string[] { "CM", "PE  " + textBox4.Text });
         }
 
     }
