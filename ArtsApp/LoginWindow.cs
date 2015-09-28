af934e99f6bc1cc9cdf3351b1a21ae1d91b6a811
Arthur Brink <arthur.brink1@gmail.com>
2015-09-28 12:30:25 +0200

    Split Application + added loginDIalog

---
+++ ArtsApp/LoginWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArtsApp
+{
+    public partial class LoginWindow : Form
+    {
+        public bool done { get; set; } = false;
+        
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public String returnUsername()
+        {
+            return username.ToString();
+        }
+
+        public String returnPassWord()
+        {
+            return password.ToString();
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            done = true;
+
+            this.Close();
+        }
+    }
+}
